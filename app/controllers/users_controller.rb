d338bf6c211b39e116b3129fe4e87c12a5b8c651
daiyada <shunsuke07l6@gmail.com>
2019-11-02 14:46:03 +0900

    ユーザー周りで必要なルーティングやコントローラを設定しました。

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  before_action :move_to_index, except: :index
+
+  def index
+  end
+
+  private
+  def move_to_index
+    redirect_to root_path unless user_signed_in?
+  end
+end
