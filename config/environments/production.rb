6161336f0bccdc436c6fed23cf15dc9cffd40985
daiyada <shunsuke07l6@gmail.com>
2019-11-09 12:24:15 +0900

    本番環境でエラーメッセージが表示されるようにしました。

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true   #本番環境でエラーが表示される
   config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
