2888586533fcf6343b126b11fea2bf9267b19fbc
daiyada <shunsuke07l6@gmail.com>
2019-11-09 18:51:34 +0900

    使い方ページのマークアップをしました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,9 @@ class UsersController < ApplicationController
     @player = Player.where(user_id: current_user&.id)
   end
 
+  def method
+  end
+
   private
   def move_to_index
     redirect_to root_path unless user_signed_in?
